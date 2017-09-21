f6dacaf8d50a9b5dc0d7b6733f4757bec268bad1
Alpha <mos.pejo@gmail.com>
2017-09-21 13:19:48 +0430

    Mohsen

--- app/src/main/java/com/example/deltasix/Confirm.java
+++ app/src/main/java/com/example/deltasix/Confirm.java
@@ -3,15 +3,11 @@ package com.example.deltasix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main3Activity extends AppCompatActivity {
+public class Confirm extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
-        /*tst21*/
-        /*Mohsen202020*/
-        /*mehrdad 22*/
-        /*Mohsen211*/
+        setContentView(R.layout.activity_confirm);
     }
 }
--- app/src/main/java/com/example/deltasix/Login.java
+++ app/src/main/java/com/example/deltasix/Login.java
@@ -3,11 +3,11 @@ package com.example.deltasix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_login);
     }
 }
