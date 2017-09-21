a354f2d32d7b2e010912835afea851b1ff4a13b3
Alpha <mos.pejo@gmail.com>
2017-09-21 11:19:07 +0430

    Mohsen Commit

--- app/src/main/java/com/example/deltasix/Main3Activity.java
+++ app/src/main/java/com/example/deltasix/Main3Activity.java
@@ -3,12 +3,11 @@ package com.example.deltasix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        /*Test Delta Six */
+        setContentView(R.layout.activity_main3);
     }
 }
