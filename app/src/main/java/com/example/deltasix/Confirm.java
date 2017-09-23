b9d962ca4a63ec8531fe4e2f2a9e7e7d5bc7c52e
Alpha <mos.pejo@gmail.com>
2017-09-23 16:15:46 +0330

    Mohsen Forouzandeh

--- app/src/main/java/com/example/deltasix/Confirm.java
+++ app/src/main/java/com/example/deltasix/Confirm.java
@@ -2,6 +2,7 @@ package com.example.deltasix;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Confirm extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class Confirm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            if(extras.containsKey("Mobile")){
+                String sMobile = extras.getString("Mobile");
+
+                Toast.makeText(getApplicationContext(), sMobile, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/deltasix/Login.java
+++ app/src/main/java/com/example/deltasix/Login.java
@@ -1,7 +1,12 @@
 package com.example.deltasix;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +14,30 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView txtMobile = (TextView) findViewById(R.id.txtMobileNumber);
+                String check = txtMobile.getText().toString();
+
+                    if (!check.matches(getString(R.string.RexMobile))) {
+                        Toast.makeText(Login.this, getString(R.string.Mobile_No), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(Login.this, "شماره موبایل معتبر هست", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), Confirm.class);
+                        String sMobile = txtMobile.getText().toString();
+                        intent.putExtra("Mobile", sMobile);
+                        startActivity(intent);
+                    }
+                /*TextView txtMobile = (TextView) findViewById(R.id.txtMobileNumber);
+                Intent intent = new Intent(getApplicationContext(),Confirm.class);
+                String sMobile =  txtMobile.getText().toString();
+                intent.putExtra("Mobile",sMobile);
+                startActivity(intent);
+                startActivity(new Intent(Login.this,Confirm.class));*/
+            }
+        });
     }
 }
